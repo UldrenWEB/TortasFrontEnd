41b4878799d4ece437173dff51d387a80412c1de
enmarcm <theenmanuel123@gmail.com>
2023-11-20 22:10:37 -0400

    si

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,27 +1,42 @@
+import Home from './Home'
 import MyRoute from "./MyRoute";
 import { Route, Routes } from "react-router-dom";
 
 const Content = ({ darkMode }) => {
   return (
-
     //TODO: MODIFICAR AQUI ESTO QUE LOS ELEMENTOS USAN
     //TODO: ADEMAS RECORDAR QUE TODOS LOS COMPONENTES QUE CARGUEN DEBERAN USAR SU WITDH 100%
-    <Routes className={'main-layout'}>
+    <Routes className={"main-layout"}>
       <Route
         path="/"
         element={
-          <div className={darkMode ? "darkMode" : "lightMode"} style={{width: '100%'}}>Path</div>
+          <Home
+            className={darkMode ? "darkMode" : "lightMode"}
+            style={{ width: "100%" }}
+          >
+            Path
+          </Home>
         }
       />
       <Route
         path="/home"
         element={
-          <div className={darkMode ? "darkMode" : "lightMode"} style={{width: '100%'}}>Home</div>
+          <div
+            className={darkMode ? "darkMode" : "lightMode"}
+            style={{ width: "100%" }}
+          >
+            Home
+          </div>
         }
       />
       <Route
         path="/prueba"
-        element={<MyRoute className={darkMode ? "darkMode" : "lightMode"} style={{width: '100%'}}/>}
+        element={
+          <MyRoute
+            className={darkMode ? "darkMode" : "lightMode"}
+            style={{ width: "100%" }}
+          />
+        }
       />
     </Routes>
   );
