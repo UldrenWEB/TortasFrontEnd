aae4a127f06a87b6cf1603637d8e6486cd4b7447
enmarcm <theenmanuel123@gmail.com>
2023-11-27 01:43:55 -0400

    inicio

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -20,7 +20,7 @@ import CreatePayMethod from "../BO/CreatePayMethod";
 import ChangeStatusSeller from "../BO/ChangeStatusSeller";
 import SuperLoader from "./SuperLoader";
 import AsignarLocalVendedor from "../BO/AsignarLocalVendedor";
-import CreateBilling from '../BO/CreateBilling'
+import CreateBilling from "../BO/CreateBilling";
 import AsingPayToBill from "../BO/payBill";
 import EditPerson from "../BO/EditPerson";
 import AsignarSalarioVendedor from "../BO/AsignarSalarioVendedor";
@@ -88,19 +88,31 @@ const Content = ({
           }
         />
 
-        {isLogged ? <Route path="/home" element={<HomeLogged setLoading={setIsLoading} infoUser={dataUser}/>} /> : null}
+        <Route
+          path="/home"
+          element={<HomeLogged setLoading={setIsLoading} infoUser={dataUser} />}
+        />
 
         {/* Estas se pueden hacer dinamicas */}
-        <Route path="/person/control/createPerson" element={<CreatePerson setLoading={setIsLoading} />} />
+        <Route
+          path="/person/control/createPerson"
+          element={<CreatePerson setLoading={setIsLoading} />}
+        />
 
         <Route
           path="/sales/products/createProduct"
           element={<CreateProduct setLoading={setIsLoading} />}
         />
 
-        <Route path="/local/control/createRoute" element={<CreateRoute setLoading={setIsLoading} />} />
+        <Route
+          path="/local/control/createRoute"
+          element={<CreateRoute setLoading={setIsLoading} />}
+        />
 
-        <Route path="/local/control/createLocal" element={<CreateLocal setLoading={setIsLoading} />} />
+        <Route
+          path="/local/control/createLocal"
+          element={<CreateLocal setLoading={setIsLoading} />}
+        />
 
         <Route
           path="/billing/payMethod/createMethod"
@@ -124,8 +136,8 @@ const Content = ({
 
         <Route
           path="/billing/bill/createBilling"
-          element={<CreateBilling setLoading={setIsLoading}
-          />} />
+          element={<CreateBilling setLoading={setIsLoading} />}
+        />
 
         <Route
           path="/billing/bill/payBill"
